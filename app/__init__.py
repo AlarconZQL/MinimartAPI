6d69ea5d8c3da9b43eac2157256fdfd967bcfcdb
Ezequiel Alarcon <alarcon.zql@gmail.com>
2020-07-18 17:55:07 -0300

    Added database configuration and models package. Integrate database instance on the app's factory method

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from .extensions import db
+
 
 def create_app():
     """Create main application"""
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///coco-minimart.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    """Initialize plugins"""
+    db.init_app(app)
     with app.app_context():
+        db.create_all()
         return app
